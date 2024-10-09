97d3cabbf7627b3c22d45069db1c8b6e88c885c0
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-10-09 12:19:58 +0530

    Implementing into the Web using Streamlit

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 from DNAToolkit import *
 from utilities import colored
 import random
+import streamlit as st
 
-randDNAStr = ''.join([random.choice(Nucleotides)
-                     for nuc in range(50)])
-
+randDNAStr = ''.join([random.choice(Nucleotides) for nuc in range(50)])
 validatedDNAStr = validateSeq(randDNAStr)
-print(f'\nSequence: {colored(validatedDNAStr)}')
-print(f'\n(1) - Sequence Length: {len(validatedDNAStr)}')
-print(colored(f'\n(2) - Nucletide Frequency: {countNucliotideFrequency(validatedDNAStr)}'))
-print(f'\n(3) - DNA/RNA Transcription: {colored(transcription(validatedDNAStr))}')
 
-print(f'\n(4) - DNA String + Reverse Complement:')
-print(f'\n5` {colored(validatedDNAStr)} 3`')
-print(f"\n   {''.join(['|' for c in range(len(validatedDNAStr))])}")
-print(f'\n3` {colored(reverse_complement(validatedDNAStr))} 5`')
+st.markdown(f'**Sequence:** {colored(validatedDNAStr)}', unsafe_allow_html=True)
+
+st.write(f'(1) - Sequence Length: {len(validatedDNAStr)}')
+
+nucleotide_frequency = countNucliotideFrequency(validatedDNAStr)
+
+st.markdown(f'(2) - Nucleotide Frequency: {colored(str(nucleotide_frequency))}', unsafe_allow_html=True)
+
+st.markdown(f'(3) - DNA/RNA Transcription: {colored(transcription(validatedDNAStr))}', unsafe_allow_html=True)
+
+st.markdown(f'(4) - DNA String + Reverse Complement:', unsafe_allow_html=True)
+
+st.markdown(f"5' &nbsp;&nbsp;{colored(validatedDNAStr)} &nbsp;&nbsp;3'", unsafe_allow_html=True)
+
+st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&thinsp;&thinsp;{'&thinsp;'.join(['|&emsp13;' for _ in range(len(validatedDNAStr))])}", unsafe_allow_html=True)
+
+st.markdown(f"3' &nbsp;&nbsp;{colored(reverse_complement(validatedDNAStr))} &nbsp;&nbsp;5'", unsafe_allow_html=True)
+
+
+
+# from DNAToolkit import *
+# from utilities import colored
+# import random
+# import streamlit as st
+
+# randDNAStr = ''.join([random.choice(Nucleotides)
+#                      for nuc in range(50)])
+
+# validatedDNAStr = validateSeq(randDNAStr)
+# print(f'\nSequence: {colored(validatedDNAStr)}')
+# st.write(f'\nSequence: {colored(validatedDNAStr)}')
+# print(f'\n(1) - Sequence Length: {len(validatedDNAStr)}')
+# print(colored(f'\n(2) - Nucletide Frequency: {countNucliotideFrequency(validatedDNAStr)}'))
+# print(f'\n(3) - DNA/RNA Transcription: {colored(transcription(validatedDNAStr))}')
+
+# print(f'\n(4) - DNA String + Reverse Complement:')
+# print(f'\n5` {colored(validatedDNAStr)} 3`')
+# print(f"\n   {''.join(['|' for c in range(len(validatedDNAStr))])}")
+# print(f'\n3` {colored(reverse_complement(validatedDNAStr))} 5`')
 
--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-def colored(seq):
+"""def colored(seq):
     bcolors = {
         'A': '\033[92m',
         'C': '\033[94m',
@@ -17,3 +17,44 @@ def colored(seq):
             tmpStr += bcolors['reset'] + nuc
 
     return tmpStr + '\033[0;0m'
+"""
+
+# def colored(seq):
+#     color_map = {
+#         'A': 'lightgreen',
+#         'C': 'lightblue',
+#         'G': 'lightorange',
+#         'T': 'lightred',
+#         'U': 'lightred',
+#     }
+
+#     tmpStr = ""
+
+#     for nuc in seq:
+#         if nuc in color_map:
+#             tmpStr += f'<span style="color:{color_map[nuc]}">{nuc}</span>'
+#         else:
+#             tmpStr += f'<span>{nuc}</span>'
+
+#     return tmpStr
+
+
+def colored(seq):
+    bcolors = {
+        'A': '<span style="color: #00FF00; font-size: 20px;">A</span>',  # Bright green
+        'C': '<span style="color: #0000FF; font-size: 20px;">C</span>',  # Bright blue
+        'G': '<span style="color: #FFFF00; font-size: 20px;">G</span>',  # Bright yellow
+        'T': '<span style="color: #FF4500; font-size: 20px;">T</span>',  # Bright orange-red
+        'U': '<span style="color: #FF4500; font-size: 20px;">U</span>',  # Bright orange-red
+        'reset': '</span>'
+    }
+
+    tmpStr = ""
+
+    for nuc in seq:
+        if nuc in bcolors:
+            tmpStr += bcolors[nuc]
+        else:
+            tmpStr += bcolors['reset'] + nuc
+
+    return tmpStr + bcolors['reset']
