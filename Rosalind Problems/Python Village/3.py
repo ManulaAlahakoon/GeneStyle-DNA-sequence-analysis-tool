a2b405f3a48160577850223e24b7c55ce0e18703
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-10-30 13:55:59 +0530

    File structure update

--- Rosalind Problems/Python Village/3.py
+++
@@ -1,4 +0,0 @@
-
-a ='DH5Zu2bLutraiRYdd538G0LIBujlEr2CDfXWwzjKiF7rmAGhS3w29wBXoCrSa9Rs1aXu43Mh8s5k0hNaUVQDZUglPGOeOwislizeniZWWjBYddp1nQVzqWW9bSfPfP4AXLwEuVCEVmDyHbsdViDdGWkoAx5sRLxQbGTC1zrc2k1Q.'
-
-print (a[7:12] +' '+ a[93:102])
